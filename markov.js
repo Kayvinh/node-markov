67f61d64209d57949f9ad7541b3f2094e616628e
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-04-04 16:43:14 -0700

    need to refactor to use Map

--- markov.js
+++ markov.js
@@ -16,8 +16,10 @@ class MarkovMachine {
    *
    *  For text of "The cat in the hat.", chains will be:
    * 
+   *  [ 'the', 'cat', 'in', 'the', 'hat' ]
+   * 
    *  {
-   *   "The": ["cat"],
+   *   "The": ["cat"],    ["cat","hat"]
    *   "cat": ["in"],
    *   "in": ["the"],
    *   "the": ["hat."],
@@ -28,6 +30,23 @@ class MarkovMachine {
 
   getChains() {
     // TODO: implement this!
+    const markovMap = {}
+
+
+    //TODO: store this.words[i] in variable
+    //TODO: refactor and use Map
+    for (let i = 0; i < this.words.length; i++) {
+      const nextWord = (i === this.words.length - 1) ? null : this.words[i + 1]
+
+      if (this.words[i] in markovMap) {
+        markovMap[this.words[i]].push(nextWord);
+      } else {
+        markovMap[this.words[i]] = [nextWord];
+      }
+
+    }
+
+    return markovMap;
   }
 
 
@@ -40,5 +59,30 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    // Create array with first word in input text
+    const text = [this.words[0]];
+
+    let lastWord = text[text.length - 1];
+
+    const markovMap = this.getChains();
+
+    while (lastWord !== null) {
+      const randomChainIndex = Math.floor(Math.random() * markovMap[lastWord].length)
+      text.push(markovMap[lastWord][randomChainIndex]);
+      lastWord = text[text.length - 1];
+    }
+
+    return text.join(" ");
+
   }
 }
+
+// const catInHatMachine = new MarkovMachine("the cat in the hat");
+// console.log(catInHatMachine.words);
+// console.log(catInHatMachine.getChains());
+// console.log(catInHatMachine.getText());
+
+module.exports = {
+  MarkovMachine,
+};
